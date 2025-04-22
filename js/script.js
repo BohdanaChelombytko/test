5a51d0a108841f441e8af94f5ff04d2e9d00ccf7
Bohdana Chelombytko <b.chelombytko@omisoft.net>
2025-04-22 08:29:43 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+// alert("Hello");
+// const result = confirm("Are you here?");
+// console.log(result);
+// const answer = prompt("Are you 18?", "18");
+// console.log(typeof (answer));
+
+
+// const answers = [];
+// answers[0] = prompt("What is your first name?", "");
+// answers[1] = prompt("What is your second name?", "");
+// answers[2] = prompt("How old are you", "");
+// document.write(answers);
+
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
+
